bd454e13d9b2293fba38c85e9854088ee09af9bf
cynthiangel <118499700+cynthiangel@users.noreply.github.com>
2023-02-08 19:25:45 +0700

    Add files via upload

---
+++ 3_D_71220858.py
@@ -0,0 +1,12 @@
+print('##  Program Python Segitiga Bintang  ##')
+print('=======================================')
+print()
+ 
+tinggi_segitiga = int(input('Input tinggi segitiga: '))
+print()
+ 
+for i in range(tinggi_segitiga):
+  for j in range(i+1):
+    print(' *',end='')
+  print()
+Hasil kode program:
